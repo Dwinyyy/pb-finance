222f00de59a86b06a6f8d955e524df325f209f0f
Dwinyyy <aldwin.gotingco475@gmail.com>
2026-05-29 03:15:07 +0800

    chore: clean up ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['.vercel', 'dist', 'smart_*.js', 'update_*.js']),
+  globalIgnores(['.vercel', 'dist']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
